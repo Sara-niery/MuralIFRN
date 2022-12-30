067b9d3bb4c4479319084b160a74e6baa7fd2331
Sara-niery <nierysantos@hotmail.com>
2022-12-30 07:48:48 -0300

    .

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Permission
 from .models import Usuario,Coordenacao,Aviso
 from .forms import CoordenacaoForm,UsuarioForm,AvisoForm
 
